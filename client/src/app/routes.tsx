4a338d587a6f50c04b1b7df897957c5d6bfe1d56
edeguzman05 <157542244+edeguzman05@users.noreply.github.com>
2026-05-04 12:26:02 -0700

    Add AboutPage route to appRoutes

--- client/src/app/routes.tsx
+++ client/src/app/routes.tsx
@@ -2,10 +2,12 @@ import HomePage from "./pages/HomePage";
 import AreaPage from "./pages/AreaPage";
 import FireMapPage from "./pages/FireMapPage";
 import FireAreaPage from "./pages/FireAreaPage";
+import AboutPage from "./pages/AboutPage";
 
 export const appRoutes = [
   { path: "/", element: <HomePage /> },
   { path: "/us/:slug", element: <AreaPage /> },
   { path: "/fire", element: <FireMapPage /> },
   { path: "/fire/:slug", element: <FireAreaPage /> }
+  { path: "/about", element: <AboutPage /> }
 ];
